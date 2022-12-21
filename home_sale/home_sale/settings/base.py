fa9be65462f2e83c2f440961cd63761575e98439
sizan378 <sizanmahmud08@gmail.com>
2022-12-21 09:15:34 +0600

    logging add in base file

--- home_sale/home_sale/settings/base.py
+++ home_sale/home_sale/settings/base.py
@@ -142,3 +142,51 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import logging
+import logging.config
+
+from django.utils.log import DEFAULT_LOGGING
+logger = logging.getLogger(__name__)
+
+LOG_LEVEL = "INFO"
+
+# %(asctime)s %(name)-12s %(levelname)-8s %(message)s
+# asctime = define your timezone
+# name = package name
+# levelname = log level of message
+# message = log message
+# 12s & 8s = format specifications
+
+logging.config.dictConfig({
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters":{
+        "console":{
+            "format":"%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+        },
+        "file":{"format":"%(asctime)s %(name)-12s %(levelname)-8s %(message)s",},
+        "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
+    },
+    "handlers":{
+        "console":{
+            "class":"logging.StreamHandler",
+            "formatter":"console",
+
+        },
+        "file":{
+            "level":"INFO",
+            "class":"logging.FileHandler",
+            "formatter":"file",
+            "filename":"logs/home_sale.log",
+        },
+        "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
+    },
+    "loggers":{
+        "": {"level":"INFO", "handlers": ["console","file"],"propagate":False},
+        "apps": {"level": "INFO", "handlers": ["console"],"propagate":False},
+        "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
+    }
+
+})

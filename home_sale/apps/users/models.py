e464b3be424fccc4331b70776f769a43711f2abc
sizan378 <sizanmahmud08@gmail.com>
2022-12-22 09:43:50 +0600

    date_joined updated

--- home_sale/apps/users/models.py
+++ home_sale/apps/users/models.py
@@ -16,7 +16,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    date_joined = models.DateTimeField(auto_now_add=True)
+    date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username','first_name','last_name']

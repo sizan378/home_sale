1ecce032a16782d30deca004a52905a2fdd29398
sizan378 <sizan378>
2022-12-27 08:05:07 +0600

    🐞 fix(views.py): enquiries
    permission class name fixed

--- home_sale/apps/enquiries/views.py
+++ home_sale/apps/enquiries/views.py
@@ -2,13 +2,13 @@ from django.core.mail import send_mail
 
 from home_sale.settings.development import DEFAULT_FROM_EMAIL
 from rest_framework import permissions
-from rest_framework.decorators import api_view, permissions_classes
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
 from .models import Enquire
 
 @api_view(['POST'])
-@permissions_classes((permissions.IsAuthenticated,))
+@permission_classes((permissions.IsAuthenticated,))
 def send_enquiry_email(request):
     data = request.data 
 

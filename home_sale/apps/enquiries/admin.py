73e9757886259f69fe42996b7dde2d7a121c1ca1
sizan378 <sizan378>
2022-12-28 08:22:29 +0600

    this commit change after file location chage

--- home_sale/apps/enquiries/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Enquire
-
-
-class EnquireAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'phone_number')
-
-admin.site.register(Enquire, EnquireAdmin)
--- home_sale/apps/properties/exceptions.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.exceptions import APIException
-
-
-class PropertyNotFound(APIException):
-    status_code = 404
-    default_detail = 'The requested Property not found.'
